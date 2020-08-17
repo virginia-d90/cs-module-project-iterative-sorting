17f11bbe92ad5953a316ad8d534ad56642f48980
Virginia Davenport <virginialdport@gmail.com>
2020-08-17 16:03:53 -0600

    made edit to challenge.py

--- src/challenge.py
+++ src/challenge.py
@@ -15,7 +15,7 @@
 array = [85, 46, 27, 81, 94, 9, 27, 38, 43, 99, 37, 63, 31, 42, 14]
 def divide(arr):
     for i in arr:
-        if i % 3:
+        if i % 3 == 0:
             print(i)
 
 divide(array)
